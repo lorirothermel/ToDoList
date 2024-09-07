bfeefb84eb4dca4aed7de40b8ef61974965eacfc
lorirothermel <lori@rothermel1.com>
2024-09-07 15:09:18 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lori Rothermel on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
